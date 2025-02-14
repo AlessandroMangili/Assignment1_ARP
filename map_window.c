dcf5e450fe2daefbb3b5045f5b080e66176aa1b7
AlessandroMangili <alessandro.mangili1@studenti.unimi.it>
2025-02-14 16:00:03 +0100

    Fix map dimension and border color

--- map_window.c
+++ map_window.c
@@ -200,7 +200,7 @@ int main(int argc, char *argv[]) {
     noecho();
     curs_set(0);
 
-    init_pair(1, COLOR_BLUE, COLOR_BLACK);
+    init_pair(1, COLOR_WHITE, COLOR_BLACK);
     init_pair(2, COLOR_GREEN, COLOR_BLACK);
     init_pair(3, COLOR_RED, COLOR_BLACK);
     init_pair(4, COLOR_YELLOW, COLOR_BLACK);
